853f9337b7b3b9818702f2546722b07bddf4d8b7
Ken Masumitsu <ken.masumitsu@gmail.com>
2024-02-26 14:02:32 +0900

    Initial two two_dimensional_scrollables sample

---
+++ android/app/src/main/kotlin/com/example/sample_two_dimentional_scollables/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sample_two_dimentional_scollables
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:two_dimensional_scrollables/two_dimensional_scrollables.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: TableView.builder(
+        columnCount: 10,
+        rowCount: 20,
+        columnBuilder: _buildColumnSpan,
+        rowBuilder: _buildRowSpan,
+        cellBuilder: _buildCell,
+        //pinnedRowCount: 1,
+        //pinnedColumnCount: 1,
+      ),
+    );
+  }
+
+  TableSpan _buildColumnSpan(int index) {
+    return TableSpan(
+      extent: const FixedTableSpanExtent(100),
+      backgroundDecoration: TableSpanDecoration(
+        color: index.isEven ? Colors.redAccent[100] : Colors.white,
+      ),
+      foregroundDecoration: const TableSpanDecoration(
+        border: TableSpanBorder(
+          trailing: BorderSide(
+            color: Colors.red,
+            width: 2,
+            style: BorderStyle.solid,
+          ),
+        ),
+      ),
+    );
+  }
+
+  TableSpan _buildRowSpan(int index) {
+    return TableSpan(
+      extent: const FixedTableSpanExtent(100),
+      backgroundDecoration: TableSpanDecoration(
+        color: index.isEven ? Colors.blueAccent[100] : Colors.white,
+      ),
+      foregroundDecoration: const TableSpanDecoration(
+        border: TableSpanBorder(
+          trailing: BorderSide(
+            color: Colors.blue,
+            width: 2,
+            style: BorderStyle.solid,
+          ),
+        ),
+      ),
+    );
+  }
+
+  TableViewCell _buildCell(BuildContext context, TableVicinity vicinity) {
+    return TableViewCell(
+      child: Center(
+        child: Text('Tile c: ${vicinity.column}, r: ${vicinity.row}'),
+      ),
+    );
+  }
+}
